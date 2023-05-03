657464eb53cf71e2700113a5f874f672c0f82d6d
Ani <ani.mkrtchyan1982@gmail.com>
2023-05-03 17:03:42 +0400

    file uploading

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,38 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
         return new Profile();
+    }
+
+    private String[] readFile(File file) {
+        String[] finalOutput = null;
+        String absolutePath = file.getAbsolutePath();
+
+        try (FileInputStream inputStream = new FileInputStream(absolutePath);) {
+            finalOutput = new String[4];
+
+            int c, count = 0;
+            while ((c = inputStream.read()) != -1) {
+                if (c == 10) {
+                    count++;
+                    continue;
+                }
+
+                finalOutput[count] = finalOutput[count] + (char) c;
+
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return finalOutput;
     }
 }
